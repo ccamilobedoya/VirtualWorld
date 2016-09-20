b4a16ffa727b3dae51ba64ecc68c7ade17b8410f
Saureth <aerials_001@hotmail.com>
2016-09-19 19:47:34 -0500

    borrados scripts que no están funcionando, comentadas las líneas de código

--- Assets/Scripts/VistaScript.cs
+++ Assets/Scripts/VistaScript.cs
@@ -2,26 +2,41 @@
 using System.Collections;
 using System.Collections.Generic;
 
+public enum cosas {
+	algo = 1,
+	algo2 = 2
+}
 
 public class VistaScript : MonoBehaviour {
 
-
+	public cosas cositas;
 	public float anchoVision;
 	public float distanciaVision;
 	public int cantidadRaycast;
-	public List<GameObject> objetosVisibles;
-	public List<GameObject> objetosVistos;
+//	public List<CaracteristicaScript> objetosVisibles;
+//	public List<CaracteristicaScript> objetosVistos;
+
+	public bool raycastInmovil;
+	public Transform[] apuntadoresMoviles;
+
+//	public DudaCaracteristicaScript carac;
+
+
 
 	void Start () {
-		
+//		objetosVisibles = new List<CaracteristicaScript>();
+		//if (cositas == cosas.algo) 
 	}
 	
 
 	void Update () {
-		Raycasting ();
+		if (raycastInmovil)
+			Raycasting ();
+		else
+			RaycasteoMovil ();
 	}
 
-	void Raycasting () {
+	void Raycasting () {/*
 		objetosVisibles.Clear ();
 		float movimientoMirar = (anchoVision * 2) / cantidadRaycast;
 		for (int i = 0; i < cantidadRaycast; i++) {
@@ -33,6 +48,23 @@ public class VistaScript : MonoBehaviour {
 				if (!objetosVistos.Contains (hit.transform.gameObject)) {
 					objetosVistos.Add(hit.transform.gameObject);
 				}
+			}
+		}*/
+	}
+
+	void RaycasteoMovil () {
+//		objetosVisibles.Clear ();
+		RaycastHit hit;
+		for (int i = 0; i < apuntadoresMoviles.Length; i++) {
+			
+			Debug.DrawRay (transform.position, apuntadoresMoviles[i].position - transform.position, Color.magenta);
+
+			if (Physics.Raycast (transform.position, (apuntadoresMoviles[i].position - transform.position).normalized, out hit, Vector3.Distance(apuntadoresMoviles[i].position, transform.position))) {
+//				objetosVisibles.Add (carac.ObtenerCaracteristica(Vector3.Distance(apuntadoresMoviles[i].position, transform.position), hit.transform));
+				/*if (!objetosVistos.Contains (hit.transform.gameObject)) {
+					objetosVistos.Add(hit.transform.gameObject);
+
+				}*/
 			}
 		}
 	}
